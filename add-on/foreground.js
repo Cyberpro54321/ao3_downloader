05a22d703d0477e8aa8b84190a22ab1d37fa5dd2
Cyberpro123 <rebycorp321@gmail.com>
2025-02-18 06:54:50 -0800

    streamlined

--- add-on/foreground.js
+++ add-on/foreground.js
@@ -4,9 +4,8 @@ for (let i = 0; i < x.length; i++) {
 	y[i] = x[i].innerHTML;
 }
 
-console.log("Hello World!");
 if (y.length > 0) {
-  window.alert(y.length+" Workskin(s) Detected");
+  console.log(y.length+" Workskin(s) Detected");
 } else {
 	y[0] = "";
 }
