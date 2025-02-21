d34aacf2613d7a7bdc4bc6f42d94c60cd758d4bb
Cyberpro123 <rebycorp321@gmail.com>
2025-02-20 21:18:47 -0800

    mozilla does NOT like text in version numbers

--- app/process_fic.py
+++ app/process_fic.py
@@ -134,7 +134,7 @@ if doHtmlTagChanges:
         h2location = buffer.index('<div id="chapters" class="userstuff">\n')
     buffer.pop(h2location - 2)
 
-buffer.append("<!-- This file written by AO3 Downloader version 1.0.5-alpha-0 -->")
+buffer.append("<!-- This file written by AO3 Downloader version 1.0.5.1 -->")
 
 
 for line in buffer:
